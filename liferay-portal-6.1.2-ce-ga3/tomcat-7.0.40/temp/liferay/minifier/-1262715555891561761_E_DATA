9d006b378d977b6d135272bee47967e3f7013d11
shyambhadauria <shyam.bhadauria@globallogic.com>
2014-01-10 22:56:39 +0530

    adding code on 10 jan 14..UI structure

--- liferay-portal-6.1.2-ce-ga3/tomcat-7.0.40/temp/liferay/minifier/-1262715555891561761_E_DATA
+++
@@ -1,1 +0,0 @@
-AUI().ready(function(){});Liferay.Portlet.ready(function(a,b){});Liferay.on("allPortletsReady",function(){});
--- liferay-portal-6.1.2-ce-ga3/tomcat-7.0.40/temp/liferay/ruby/bin/sass.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"jruby.jar" "L:/projects/github/liferay-portal/portal-impl/20120913155215675/output/bin/sass" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"jruby.jar" "%~dpn0" %*
